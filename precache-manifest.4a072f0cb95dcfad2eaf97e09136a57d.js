efbab28bd71d39bda15ef9ea8ea548d1000990e0
marcoSven <me@marcosven.com>
2021-09-22 07:25:03 -0700

    Updates

---
+++ precache-manifest.4a072f0cb95dcfad2eaf97e09136a57d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b12138f8c411c52312feee3700534da3",
+    "url": "/react-use-asset-loader/index.html"
+  },
+  {
+    "revision": "d2721efa43eff88448ed",
+    "url": "/react-use-asset-loader/static/css/main.2c6a48fe.chunk.css"
+  },
+  {
+    "revision": "3be06ad5418eae43648c",
+    "url": "/react-use-asset-loader/static/js/2.d6f6d597.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/react-use-asset-loader/static/js/2.d6f6d597.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2721efa43eff88448ed",
+    "url": "/react-use-asset-loader/static/js/main.3038cfe7.chunk.js"
+  },
+  {
+    "revision": "c4db3132fe06bd94dddd",
+    "url": "/react-use-asset-loader/static/js/runtime-main.b9b33123.js"
+  }
+]);
